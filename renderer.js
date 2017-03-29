5215ce95b168ce433e2bd1c43e586926cdc7843f
salamander-mh <1906747819@qq.com>
2017-03-29 09:38:25 +0800

    添加图片

--- renderer.js
+++ renderer.js
@@ -1,4 +1,12 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var childProcess = require('child_process');
 
+var ls = childProcess.exec('cat *.js | wc', function (error, stdout, stderr) {
+   if (error) {
+     console.log(error.stack);
+     console.log('Error code: '+error.code);
+   }
+   console.log('Child Process STDOUT: '+stdout);
+});
