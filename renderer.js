56beaa9f78857e7ee627f6888961e9311e172469
salamander-mh <1906747819@qq.com>
2017-03-30 09:47:44 +0800

    完成进程启动判断

--- renderer.js
+++ renderer.js
@@ -2,9 +2,42 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 var childProcess = require('child_process');
+var Q = require('q');
 
-var ls = childProcess.exec('ps -ef | grep nginx', function (error, stdout, stderr) {
-	if (!error) {
-		console.log(stdout);
+
+var phpSwitch = document.getElementById('switch-php');
+var mysqlSwitch = document.getElementById('switch-mysql');
+
+
+var nginxProgram = {
+	target: null,
+	isRunning: function() {
+		var deferred = Q.defer();
+		childProcess.exec('ps -ef | grep nginx | wc -l', function (error, stdout, stderr) {
+			if (!error) {
+				if(stdout > 2) {
+					deferred.resolve(true);
+				} else {
+					deferred.resolve(false);
+				}
+			}
+			deferred.reject(error);
+		});
+		return deferred.promise;
+	},
+	operateSwitch: function(status) {
+		if(!this.target) {
+			this.target = document.getElementById('txt-nginx');
+		}
+		this.target.innerText = status ? ' √' : 'X';
 	}
-});
+}
+
+setInterval(() => {
+	nginxProgram.isRunning().then((res) => {
+		nginxProgram.operateSwitch(res)
+
+	}).catch((error) => {
+		console.log(error);
+	});
+}, 1500);
